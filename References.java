9269d7665c9cce83f4a7a261234e8153dd5878a3
Beren Irgat <beren.irgat@ug.bilkent.edu.tr>
2024-12-01 04:12:00 +0300

    did the references part
    added the references for all different types of sources

---
+++ References.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class References {
+    public static String book(ArrayList<String> author, String name, String publisher, String ) {
+
+    }
+}
--- Source.java
+++ Source.java
@@ -44,18 +44,53 @@ public class Source {
 
     public String references() {
         switch (type) {
+
+            //Author, A. A. (Year). **Title. Publisher.
             case book:
-                return "**" + info[0] + "**";
+            String bookAuthors = "";
+            for (int i = 0; i < authors.size(); i++) {
+                bookAuthors += authors.get(i);
+                if (i < authors.size() - 1) {
+                    bookAuthors += ", ";
+                }
+            }
+            return bookAuthors + " (" + info[1] + "). **" + info[0] + ".**" + info[2] + ".";
+
+            //Author, A. A. (Year, Month Day). Title. Publication. URL
             case article_standalone:
-                return "";
+            String articleAuthors = "";
+            for (int i = 0; i < authors.size(); i++) {
+                articleAuthors += authors.get(i);
+                if( i < authors.size()-1) {
+                    articleAuthors += ", ";
+                }
+            }
+            return articleAuthors + " (" + info[1] + ", " + info[2] + "). " + info[0] + ". **" + info[3] + "**. " + info[4] + ".";
+            
+            //Author, A. A. (Year). Title. Journal Name, Volume(Issue), pages. DOI
             case article_journal:
-                return "";
+                String journalAuthors = "";
+                for (int i = 0; i < authors.size(); i++) {
+                    journalAuthors += authors.get(i);
+                    if (i < authors.size() - 1) {
+                        journalAuthors += ", ";
+                    }
+                }
+                return journalAuthors + " (" + info[1] + "). " + info[0] + ". **" + info[2] + "**, " + info[3] + "(" + info[4] + "), " + info[5] + ". " + info[6] + ".";
+            
+            //Author/Channel. (Year, Month Day). Title [Video]. Platform. URL
             case video:
-                return "";
+                return authors.get(0) + ". (" + info[1] + ", " + info[2] + "). " + info[0] + " [Video]. **" + info[3] + "**. " + info[4] + ".";
+
+            //Director, A. A. (Director). (year). Title [Film]. Prod Company.
             case movie: 
-                return "";
+                return authors.get(0) + " (Director). (" + info[1] + "). " + info[0] + " [Film]. **" + info[2] + "**.";
+            
+            //Entry Title. (Year, Month day). In Wikipedia. URL
             case wikipedia:
-                return "";
+                return "**" + info[0] + "**. (" + info[1] + ", " + info[2] + "). In **Wikipedia**. " + info[3] + ".";
+            
+            //default
             default:
                 return "";
         }      
